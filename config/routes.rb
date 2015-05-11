0bcc774baeaab94be0287a8cfa9fc6b29d69308e
Matthew T Farley <matthewthomasfarley@gmail.com>
2015-05-11 16:54:23 -0400

    Adds devise and customizes devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
   get 'welcome/about'
+
+  devise_for :users
+  get 'about' => 'welcome#about'
   get 'welcome/index'
   root to: 'welcome#index'
 end
